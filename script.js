0fd3972ab4168e975f0d81e8985607abf1c1ee14
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-10-20 17:00:27 +0200

    Marné zápasení s animací smazáno

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-"use strict";
-console.log("Funguju!");
+'use strict';
+console.log('Funguju!');
 
-let hraje = "circle";
+let hraje = 'circle';
 
-const buttonsElm = document.querySelectorAll("button")
-const hrajeSymbolElm = document.querySelector("#change_symbol")
+const buttonsElm = document.querySelectorAll('button');
+const hrajeSymbolElm = document.querySelector('#change_symbol');
 
 const addSymbol = (e) => {
-  if (hraje === "circle") {
-    e.target.classList.add("hraci_plocha--circle")
-    hraje = "cross"
-    hrajeSymbolElm.innerHTML = `<img src="podklady/${hraje}.svg" alt="${hraje}"/>`
+  if (hraje === 'circle') {
+    e.target.classList.add('hraci_plocha--circle');
+    hraje = 'cross';
+    hrajeSymbolElm.innerHTML = `<img src="podklady/${hraje}.svg" alt="${hraje}"/>`;
     e.target.disabled = true;
   } else {
-    e.target.classList.add("hraci_plocha--cross")
-    hraje = "circle"
-    hrajeSymbolElm.innerHTML = `<img src="podklady/${hraje}.svg" alt="${hraje}"/>`
+    e.target.classList.add('hraci_plocha--cross');
+    hraje = 'circle';
+    hrajeSymbolElm.innerHTML = `<img src="podklady/${hraje}.svg" alt="${hraje}"/>`;
     e.target.disabled = true;
   }
-}
+};
 
 for (let i = 0; i < buttonsElm.length; i++) {
-  buttonsElm[i].addEventListener("click", addSymbol)
+  buttonsElm[i].addEventListener('click', addSymbol);
 }
