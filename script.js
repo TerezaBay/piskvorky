87fd40e41d75ace6b1fadbfb8c4f90420f84fcc6
Tereza Bayerová <49003875+TerezaBay@users.noreply.github.com>
2020-10-31 22:32:05 +0100

    Diagonály se počítají jen do 3

--- script.js
+++ script.js
@@ -27,19 +27,19 @@ buttonsElm.forEach((button) => {
 
     // vyskakovací okno s výsledkem
     const confirmQ = (winMessage) => {
-      const askAgain = "Spustit novou hru?"
+      const askAgain = 'Spustit novou hru?';
       if (confirm(`${winMessage} ${askAgain}`)) {
-        location.reload()
-      } else null
-    }
+        location.reload();
+      } else null;
+    };
 
     const alertWinner = () => {
       if (isWinningMove(e.target) === true) {
         if (getSymbol(e.target) === 'circle') {
-          let winMessage = `Vyhrálo kolečko!`
-          confirmQ(winMessage)
+          let winMessage = `Vyhrálo kolečko!`;
+          confirmQ(winMessage);
         } else {
-          let winMessage = `Vyhrál křížek!`
+          let winMessage = `Vyhrál křížek!`;
           confirmQ(winMessage);
         }
       }
@@ -67,8 +67,8 @@ const getPosition = (button) => {
   };
 };
 
-// getButton pro číslo řádku a sloupce vrátí prvek
-const getButton = (row, column) => buttonsElm[row * boardSize + column];
+// getField pro číslo řádku a sloupce vrátí prvek
+const getField = (row, column) => buttonsElm[row * boardSize + column];
 
 // getSymbol, pro políčko s křížkem vrátí řetězec "cross" a pro kolečko "circle"
 const getSymbol = (button) => {
@@ -86,50 +86,75 @@ const isWinningMove = (button) => {
   const symbol = getSymbol(button);
 
   let i;
-  let inRow = 1; // Jednička pro právě vybrané políčko
+  let symbolsInRow = 1; // Jednička pro právě vybrané políčko
 
   // Koukni doleva
-  i = origin.column;
-  while (i > 0 && symbol === getSymbol(getButton(origin.row, i - 1))) {
-    inRow++;
-    i--;
+  i = origin.column; // to je číslo sloupce
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    symbolsInRow += 1;
+    i -= 1;
   }
 
   // Koukni doprava
   i = origin.column;
-  while (
-    i < boardSize - 1 &&
-    symbol === getSymbol(getButton(origin.row, i + 1))
-  ) {
-    inRow++;
-    i++;
+  while (i < boardSize - 1 && symbol === getSymbol(getField(origin.row, i + 1))) {
+    symbolsInRow += 1;
+    i += 1;
   }
 
-  if (inRow >= symbolsToWin) {
+  if (symbolsInRow >= symbolsToWin) {
     return true;
   }
 
-  let inColumn = 1;
+  let symbolsInColumn = 1;
+
   // Koukni nahoru
-  i = origin.row;
-  while (i > 0 && symbol === getSymbol(getButton(i - 1, origin.column))) {
-    inColumn++;
-    i--;
+  i = origin.row; // to je číslo řady
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    symbolsInColumn += 1;
+    i -= 1;
   }
 
   // Koukni dolu
   i = origin.row;
-  while (
-    i < boardSize - 1 &&
-    symbol === getSymbol(getButton(i + 1, origin.column))
-  ) {
-    inColumn++;
-    i++;
+  while (i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))) {
+    symbolsInColumn += 1;
+    i += 1;
   }
+  console.log("symboly ve sloupci " + symbolsInColumn)
 
-  if (inColumn >= symbolsToWin) {
+  if (symbolsInColumn >= symbolsToWin) {
     return true;
   }
+
+  // Koukni diagonála "pravá"
+  let symbolsInDiagonalRight = 1;
+  let r = origin.row; // to je číslo řady
+  let c = origin.column // to je číslo sloupce
+
+  // nahoru doprava
+  while (r > 0 && c < boardSize - 1 && symbol === getSymbol(getField(r - 1, c + 1))) {
+    symbolsInDiagonalRight += 1;
+    r += 1;
+    c -= 1;
+  }
+  
+  // dolu doleva
+  while (r < boardSize - 1 && c > 0 && symbol === getSymbol(getField(r + 1, c - 1))) {
+    symbolsInDiagonalRight += 1;
+    r -= 1;
+    c += 1;
+  }
+  console.log("symboly diagonálně " + symbolsInDiagonalRight)
+  
+  if (symbolsInDiagonalRight >= symbolsToWin) {
+    return true;
+  }
+
+  // Koukni diagonála "levá"
+  // Koukni diagonálně nahoru doleva
+  // Koukni diagonálně dolů doprava
 
   return false;
 };
